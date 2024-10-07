6148bf586755bfb7cf2e6c4baeba84894a4adabb
Mustafazade Nigar Elnur <Musta_cz12@itstep.edu.az>
2024-10-07 12:08:23 +0400

    card commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
-import Image from "next/image";
 import localFont from "next/font/local";
+import CalendarIcon from "@/components/icons/CalendarIcon";
 
+// Import custom fonts
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -14,102 +15,49 @@ const geistMono = localFont({
 
 export default function Home() {
   return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/pages/index.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className={`p-6 bg-green-200 ${geistSans.variable} ${geistMono.variable}`}>
+      <h1 className="text-3xl font-semibold tracking-widest">Card</h1>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <div className="bg-white rounded-xl py-5 w-[327px] shadow-lg">
+        <div className="flex justify-between px-6 pb-4">
+          <h3 className="text-[#0D38CE] font-semibold text-sm">Хакатон</h3>
+
+          <div className="rounded-full bg-[#E4EAFF] w-6 h-6 flex items-center justify-center">
+            <CalendarIcon />
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <div className="h-px bg-[#B8C8FF] w-full" />
+
+        <div className="px-6 text-[#00000099]">
+          <div className="flex justify-between items-center mb-4">
+            <div>
+              <h3 className="text-[#000000] font-bold text-lg">More Tech</h3>
+              <p>20 октября 2021</p>
+            </div>
+            <button className="text-[#0D38CE] font-bold rounded-full border border-[#0D38CE] px-4 py-1">
+              Сайт
+            </button>
+          </div>
+
+          <ul className="text-[#0D38CE] font-bold flex justify-between gap-3 mb-4">
+            <li className="bg-[#E4EAFF] px-3 py-1 rounded-full">Дизайн</li>
+            <li className="bg-[#E4EAFF] px-3 py-1 rounded-full">Data Science</li>
+            <li className="bg-[#E4EAFF] px-3 py-1 rounded-full">AR</li>
+          </ul>
+
+          <p className="mb-4">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Odio amet
+            feugiat ut integer tincidunt sed. Fusce vulputate sed commodo, sed
+            lorem. Mi semper orci, semper vestibulum.
+          </p>
+
+          <ul className="flex justify-between gap-3">
+            <li className="text-[#0D38CE] font-bold ">Есть отбор</li>
+            <li className="text-[#0D38CE] font-bold">Регистрация до 21.09</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
